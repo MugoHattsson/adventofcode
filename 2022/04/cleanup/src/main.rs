57a341e22bb45fb997c120f45884a76532aa3774
MugoHattsson <hu5174ma-s@student.lu.se>
2022-12-05 18:03:21 +0100

    Refine solution

--- 2022/04/cleanup/src/main.rs
+++ 2022/04/cleanup/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::ops::RangeInclusive;
 
 fn main() {
     let filename = "input.txt";
@@ -9,32 +10,29 @@ fn main() {
     let mut num_overlap = 0;
 
     for line in lines {
-        let something = line
+        let line = line
             .split(",")
             .flat_map(|x| x.split("-"))
             .map(|x| x.parse::<i32>().unwrap())
             .collect::<Vec<i32>>();
 
-        let first = (something[0], something[1]);
-        let second = (something[2], something[3]);
+        let first = line[0]..=line[1];
+        let second = line[2]..=line[3];
 
         // Contained
-        if size(first) > size(second) {
-            if first.0 <= second.0 && first.1 >= second.1 {
+        if size(&first) > size(&second) {
+            if first.start() <= second.start() && first.end() >= second.end() {
                 num_contained += 1;
             }
         } else {
-            if first.0 >= second.0 && first.1 <= second.1 {
+            if first.start() >= second.start() && first.end() <= second.end() {
                 num_contained += 1;
             }
         }
 
-        let range1 = first.0..=first.1;
-        let range2 = second.0..=second.1;
-
         // Overlaps
-        for i in range1 {
-            if range2.contains(&i) {
+        for i in first {
+            if second.contains(&i) {
                 num_overlap += 1;
                 break;
             }
@@ -45,6 +43,6 @@ fn main() {
     println!("Overlaps: {num_overlap}");
 }
 
-fn size(tuple: (i32, i32)) -> i32 {
-    i32::abs(tuple.0 - tuple.1)
+fn size(range: &RangeInclusive<i32>) -> i32 {
+    range.end() - range.start()
 }
