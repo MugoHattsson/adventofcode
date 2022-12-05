75b42ceab0bf6375b01b8d0f5166dd1322a45e4d
MugoHattsson <hu5174ma-s@student.lu.se>
2022-12-05 18:51:30 +0100

    Refactor solution

--- 2022/05/supply_stacks/src/main.rs
+++ 2022/05/supply_stacks/src/main.rs
@@ -3,115 +3,78 @@ use std::fs;
 fn main() {
     let filename = "input.txt";
     let file = fs::read_to_string(filename).expect("Could not read file {file_path}");
-    let lines = file.split("\n").collect::<Vec<&str>>();
+    let lines: Vec<_> = file.split('\n').collect();
 
-    task1(&lines);
-    task2(&lines);
-}
-
-fn task1(lines: &Vec<&str>) {
-    let initial_height = lines.iter().position(|x| x.contains("1")).unwrap();
-    let num_stacks = *lines
-        .iter()
-        .find(|x| x.contains("1"))
-        .unwrap()
-        .trim()
-        .split(" ")
-        .filter(|&x| x != "")
-        .map(|x| x.parse::<usize>().unwrap())
-        .collect::<Vec<usize>>()
-        .last()
-        .unwrap();
-
-    let mut stacks: Vec<Vec<char>> = Vec::new();
-    for _ in 0..num_stacks {
-        stacks.push(Vec::new());
-    }
-
-    init_stacks(&mut stacks, lines, initial_height, num_stacks);
-
-    for line in lines[(initial_height + 1)..].iter() {
-        if *line == "" {
-            continue;
-        }
-        let line = line
-            .split(" ")
-            .flat_map(|x| x.parse::<usize>())
-            .collect::<Vec<usize>>();
-
-        let from = line[1] - 1;
-        let to = line[2] - 1;
-
-        for _ in 0..line[0] {
-            let x = stacks[from].pop().unwrap();
-            stacks[to].push(x);
-        }
-    }
-
-    print!("Cratemover 9000: ");
-    for stack in stacks {
-        print!("{}", stack.last().unwrap());
-    }
-    println!("");
-}
-
-fn task2(lines: &Vec<&str>) {
-    let initial_height = lines.iter().position(|x| x.contains("1")).unwrap();
+    let stack_height = lines.iter().position(|x| x.contains('1')).unwrap();
     let num_stacks: usize = *lines
         .iter()
-        .find(|x| x.contains("1"))
+        .find(|x| x.contains('1'))
         .unwrap()
         .trim()
-        .split(" ")
-        .filter(|&x| x != "")
-        .map(|x| x.parse::<usize>().unwrap())
-        .collect::<Vec<usize>>()
+        .split(' ')
+        .filter(|&x| !x.is_empty())
+        .map(|x| x.parse::<_>().unwrap())
+        .collect::<Vec<_>>()
         .last()
         .unwrap();
 
-    let mut stacks: Vec<Vec<char>> = Vec::new();
-    for _ in 0..num_stacks {
-        stacks.push(Vec::new());
-    }
+    let mut stacks_9000: Vec<Vec<char>> = Vec::new();
+    let mut stacks_9001: Vec<Vec<char>> = Vec::new();
+    create_initial_stacks(&mut stacks_9000, &lines, stack_height, num_stacks);
+    create_initial_stacks(&mut stacks_9001, &lines, stack_height, num_stacks);
 
-    init_stacks(&mut stacks, lines, initial_height, num_stacks);
+    let procedure = lines[(stack_height + 1..)]
+        .iter()
+        .filter(|step| !step.is_empty());
 
-    for line in lines[(initial_height + 1)..].iter() {
-        if *line == "" {
-            continue;
-        }
-        let line = line
-            .split(" ")
+    // Moving strategy for cratemover 9000
+    // Move one crate at a time, changing the stack order
+    for step in procedure.clone() {
+        let line = step
+            .split(' ')
             .flat_map(|x| x.parse::<usize>())
             .collect::<Vec<usize>>();
 
         let from = line[1] - 1;
         let to = line[2] - 1;
 
-        let mut temp_stack: Vec<char> = Vec::new();
         for _ in 0..line[0] {
-            let x = stacks[from].pop().unwrap();
-            temp_stack.push(x);
-        }
-        for _ in 0..line[0] {
-            let x = temp_stack.pop().unwrap();
-            stacks[to].push(x);
+            let x = stacks_9000[from].pop().unwrap();
+            stacks_9000[to].push(x);
         }
     }
 
-    print!("Cratemover 9001: ");
-    for stack in stacks {
-        print!("{}", stack.last().unwrap());
+    // Moving strategy for cratemover 9001
+    // Move several crates at once, keeping their order
+    for step in procedure {
+        let line: Vec<usize> = step
+            .split(' ')
+            .flat_map(|x| x.parse::<usize>())
+            .collect::<Vec<usize>>();
+
+        let from = line[1] - 1;
+        let to = line[2] - 1;
+
+        let idx = stacks_9001[from].len() - line[0];
+        let crates: Vec<_> = stacks_9001[from].drain(idx..).collect();
+        for c in crates {
+            stacks_9001[to].push(c);
+        }
     }
-    println!("");
+
+    print_top_crates("9000", stacks_9000);
+    print_top_crates("9001", stacks_9001);
 }
 
-fn init_stacks(
+fn create_initial_stacks(
     stacks: &mut Vec<Vec<char>>,
-    lines: &Vec<&str>,
+    lines: &[&str],
     stack_height: usize,
     num_stacks: usize,
 ) {
+    for _ in 0..num_stacks {
+        stacks.push(Vec::new());
+    }
     let mut col: usize = 0;
     while col < num_stacks {
         let mut row = stack_height as i32;
@@ -125,4 +88,12 @@ fn init_stacks(
         }
         col += 1;
     }
+}
+
+fn print_top_crates(version: &str, stacks: Vec<Vec<char>>) {
+    print!("Cratemover {version}: ");
+    for stack in stacks {
+        print!("{}", stack.last().unwrap());
+    }
+    println!();
 }
