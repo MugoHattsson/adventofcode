51aa438b2565bcfaaeffb1877f87534b78bdad06
Hugo <hu5174ma-s@student.lu.se>
2022-12-08 21:00:58 +0100

    Add solution for day 8 2022

--- 2022/08/treetop_tree_house/src/main.rs
+++ 2022/08/treetop_tree_house/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 type Grid = Vec<Vec<u32>>;
 
 fn main() {
-    let filename = "small.txt";
+    let filename = "input.txt";
     let file = fs::read_to_string(filename).expect("Could not read file {file_path}");
     let lines = file.trim().split('\n').collect::<Vec<&str>>();
 
@@ -12,89 +12,109 @@ fn main() {
         .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
 
-    print_grid(&grid);
-
     let width: usize = grid[0].len();
     let height: usize = grid.len();
 
-    let outline_count = width * 2 + (height - 2) * 2;
-
-    println!("{outline_count}");
-
-    let mut sum = 0;
+    let mut num_visible = 0;
+    let mut scenic_scores: Vec<usize> = Vec::new();
     for row in 0..height {
         for col in 0..width {
             if visible(&grid, row, col) {
-                sum += 1;
+                num_visible += 1;
             }
-            scenic_score(&grid, row, col);
+            scenic_scores.push(scenic_score(&grid, row, col));
         }
     }
 
-    println!("{sum}");
-
-    println!(
-        "{:?}",
-        grid[0..2]
-            .iter()
-            .map(|row| row[3])
-            .rev()
-            .collect::<Vec<u32>>()
-    );
+    println!("Visible trees:    {num_visible}");
+    println!("Max scenic score: {}", scenic_scores.iter().max().unwrap());
 }
 
-fn scenic_score(grid: &Grid, row: usize, col: usize) {
+fn scenic_score(grid: &Grid, row: usize, col: usize) -> usize {
     let tree_height = grid[row][col];
 
-    let left_view = grid[row][0..col]
-        .iter()
-        .rev()
-        .map(|t| (*t < tree_height) as u32)
-        .collect::<Vec<u32>>();
+    let mut directions: Vec<Vec<u32>> = Vec::new();
+    let mut scores = Vec::new();
 
-    let score = match left_view[..] {
-        [] => 0,
-        [_] => 1,
-        [0, ..] => 1,
-        _ => left_view
+    // Up
+    directions.push(
+        grid[0..row]
             .iter()
-            .filter(|x| **x == 1)
-            .collect::<Vec<&u32>>()
-            .len(),
-    };
+            .map(|row| row[col])
+            .rev()
+            .collect::<Vec<u32>>(),
+    );
+    // Left
+    directions.push(
+        grid[row][0..col]
+            .iter()
+            .rev()
+            .map(|t| *t)
+            .collect::<Vec<u32>>(),
+    );
+    // Right
+    directions.push(
+        grid[row][col + 1..]
+            .iter()
+            .map(|t| *t)
+            .collect::<Vec<u32>>(),
+    );
+    // Down
+    directions.push(
+        grid[row + 1..]
+            .iter()
+            .map(|row| row[col])
+            .collect::<Vec<u32>>(),
+    );
 
-    print!("Score: {score}  ");
-    println!("{:?}", left_view);
+    for dir in &directions {
+        if dir.is_empty() {
+            scores.push(0);
+            continue;
+        }
+
+        let mut i: usize = 0;
+        while i < dir.len() {
+            if dir[i] >= tree_height {
+                i += 1;
+                break;
+            }
+
+            i += 1;
+        }
+        scores.push(i);
+    }
+
+    scores.iter().product::<usize>()
 }
 
 fn visible(grid: &Grid, row: usize, col: usize) -> bool {
     let tree_height = grid[row][col];
 
-    if !grid[row][0..col].iter().all(|t| *t < tree_height) {
-        if !grid[row][col + 1..].iter().all(|t| *t < tree_height) {
-            if !grid[0..row]
-                .iter()
-                .map(|row| row[col])
-                .all(|t| t < tree_height)
-            {
-                if !grid[row + 1..]
-                    .iter()
-                    .map(|row| row[col])
-                    .all(|t| t < tree_height)
-                {
-                    // println!("({row},{col}):  F");
-                    return false;
-                }
-            }
-        }
+    // Up
+    if grid[0..row]
+        .iter()
+        .map(|row| row[col])
+        .all(|t| t < tree_height)
+    {
+        return true;
+    }
+    // Left
+    if grid[row][0..col].iter().all(|t| *t < tree_height) {
+        return true;
+    }
+    // Right
+    if grid[row][col + 1..].iter().all(|t| *t < tree_height) {
+        return true;
+    }
+    // Down
+    if grid[row + 1..]
+        .iter()
+        .map(|row| row[col])
+        .all(|t| t < tree_height)
+    {
+        return true;
     }
 
-    // println!("({row},{col}):  T");
-    true
-}
-
-fn print_grid(grid: &Vec<Vec<u32>>) {
-    for row in grid {
-        println!("{:?}", row);
-    }
+    false
 }
